3cb7589ddb68b499ed50525c6cffdee627c74f79
s4rv4d <sarvadshetty@gmail.com>
2019-11-26 07:15:33 +0530

    Initial Commit

---
+++ ViewAndModifiers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ViewAndModifiers
+//
+//  Created by Sarvad shetty on 11/26/19.
+//  Copyright © 2019 Sarvad shetty. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
